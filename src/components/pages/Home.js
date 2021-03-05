2de6d50b5bcb2d5ee67d307f238b9a1c98533752
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-05 20:11:20 +1300

    Contact item

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
 
 const Home = () => {
     return (
-        <div>
-          Home  
+        <div className="grid-2">
+            <div>
+              {/*contact form*/}
+            </div>
+            <div>
+              <Contacts />
+            </div>
         </div>
     )
 }
---
+++ src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, {useReducer} from 'react';
+import uuid from 'uuid';
+import ContactReducer from './contactReducer';
+import ContactContext from './contactContext';
+import {
+    ADD_CONTACT, 
+    DELETE_CONTACT, 
+    SET_CURRENT, 
+    CLEAR_CURRENT, 
+    UPDATE_CONTACT, 
+    FILTER_CONTACTS, 
+    CLEAR_FILTER
+} from '../types';
+
+ const ContactState = props => {
+    const initialState ={
+        contacts: [
+            {
+                id: 1, 
+                name: 'Jill Johnson', 
+                email: 'jill@gmail.com', 
+                phone: '111-111-1111', 
+                type: 'personal'
+            },
+            {
+                id: 2, 
+                name: 'Harry Week', 
+                email: 'harry@gmail.com', 
+                phone: '098-234-231', 
+                type: 'personal'
+            },
+            {
+                id: 3, 
+                name: 'Gal Johnson', 
+                email: 'gal@gmail.com', 
+                phone: '123-123-1221', 
+                type: 'professional'
+            },
+        ], 
+
+    };
+
+    const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+    //Add contact
+
+    //Delete contact
+
+    //Set current contact
+
+    //clear current contact
+
+    //update contact
+
+    // filter contacts
+
+    // clear filter
+
+    return (
+
+        <ContactContext.Provider value={{
+            contacts: state.contacts
+        }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+
+ };
+
+ export default ContactState;
